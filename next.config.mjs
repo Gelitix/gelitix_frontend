62995b950d725b0da7bf581311ae724dd390d699
2code2code <hafidhaw@gmail.com>
2024-06-20 21:07:05 +0700

    feat: add remote pattern on next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s-light.tiket.photos",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets-bucket.tiket.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
