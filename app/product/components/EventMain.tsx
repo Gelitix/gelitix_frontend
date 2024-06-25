181b5f40328936fab2baabcebcbee9c65d0f7c76
2code2code <hafidhaw@gmail.com>
2024-06-25 14:41:49 +0700

    fix : naming component

--- app/product/components/EventMain.tsx
+++ app/product/components/EventMain.tsx
@@ -7,7 +7,7 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/BreadCrumb";
+} from "@/components/ui/Breadcrumb";
 
 const EventMain: React.FC = () => {
   return (
