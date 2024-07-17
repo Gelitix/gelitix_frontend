debc49815487c404cf16ed62a93d6d09b198177c
hizkiarss <hizkiarssihombing@gmail.com>
2024-07-17 09:30:07 +0700

    feat: order fetching

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PersonalInformation from "./components/PersonalInformation";
 import TotalPrice from "./components/TotalPrice";
 import OrderForm from "./components/OrderForm";
 import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/NavBar";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
-const page = () => {
+const Page = () => {
   const searchParams = useSearchParams();
   const eventId = searchParams.get("eventId");
   const ticketTypeId = searchParams.get("ticketTypeId");
+
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="">
       <div className=" bg-[#F4F7FE]">
@@ -23,4 +30,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
